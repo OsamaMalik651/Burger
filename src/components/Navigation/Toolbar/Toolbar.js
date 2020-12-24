27b9d0b00d084e44150a2642cc5c236655608394
Osama Malik <osama.malik013@gmail.com>
2020-12-24 22:02:41 +0500

    Made burger builder initial App

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./Toolbar.css";
+const Toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div className="">Menu</div>
+    <div className="">LOGO</div>
+    <nav>...</nav>
+  </header>
+);
+export default Toolbar;
